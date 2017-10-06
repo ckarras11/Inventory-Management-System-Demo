f9d5a291cd25bd8da8f967660e26eac11cd1936d
Christopher Karras <ckarras11@gmail.com>
2017-10-05 21:19:19 -0400

    Fixed bug for add item and vehicle where multiples were added

--- public/app.js
+++ public/app.js
@@ -8,8 +8,21 @@ const MOCK_INVENTORY = {
             item: "Rotella 15w40",
             partNumber: "oil",
             listPrice: 25.99,
+            quantityOnHand: 24,
+            reorderPoint: 6,
+            inventory_id: "vehicle1"
+
+        },
+        {
+            id: "111111",
+            image: "https://i5.walmartimages.com/asr/9a4a04d7-3059-49b1-a4fa-f5d78f43ebf2_1.c8301f7929be8b305ae0804e5ac298b0.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
+            item: "Rotella 15w40",
+            partNumber: "oil",
+            listPrice: 25.99,
             quantityOnHand: 12,
-            reorderPoint: 6
+            reorderPoint: 6,
+            inventory_id: "vehicle2"
+
         },
         {
             id: "222222",
@@ -59,6 +72,7 @@ $(function handleStart(document) {
     runReport();
     addItem();
     addVehicle();
+//    editItem();
 });
 
 function getInventoryItems(callbackfn) {
@@ -67,18 +81,18 @@ function getInventoryItems(callbackfn) {
 
 function displayInventoryItems(data) {
     for (index in data.inventory) {
-        $('#results').append(`<div class="item">
+        $('#results').append(`<div class="item" id="item">
                                     <div class="picture">
                                         <img src="${data.inventory[index].image}" alt="">
                                     </div>
                                     <div class="iteminfo">
-                                        <p>${data.inventory[index].item}</p>
+                                        <p id="name">${data.inventory[index].item}</p>
                                     </div>
                                     <div class="iteminfo">
-                                        <p>$${data.inventory[index].listPrice}</p>
+                                        <p id="price">$${data.inventory[index].listPrice}</p>
                                     </div> 
                                     <div class="iteminfo">
-                                        <p>Quantity: ${data.inventory[index].quantityOnHand}</p>
+                                        <p id="quantity">Quantity: ${data.inventory[index].quantityOnHand}</p>
                                     </div> 
                                 <div>`);
     };
@@ -108,7 +122,7 @@ function selectVehicle() {
 function reorderReport(data) {
     for (index in data.inventory) {
         if (data.inventory[index].quantityOnHand < data.inventory[index].reorderPoint) {
-            $('#reorder-list').append(`<li>${data.inventory[index].item}: ${data.inventory[index].quantityOnHand}</li>`);
+            $('#reorder-list').append(`<li>${data.inventory[index].item} Quantity: ${data.inventory[index].quantityOnHand}, Reorder Point: ${data.inventory[index].reorderPoint}</li>`);
         };
     };
 };
@@ -122,63 +136,101 @@ function runReport() {
 };
 
 function addVehicle() {
+    let modal = document.getElementById('addNewVehicle-modal');
+
     $('#add-vehicle').click(function () {
-
-        let modal = document.getElementById('addNewVehicle-modal');
         modal.style.display = "block";
+    });
 
-        $('#vehicle-form').on('click', '#submit', function (event) {
-            event.preventDefault();
-            if ($('#vehicle-input').val() !== '') {
-                $('#results').append(`<div class="item vehicle">
-                                        <div class="picture">
-                                            <img src="" alt="">
-                                        </div>
-                                        <div class="iteminfo">
-                                            <p>${$('#vehicle-input').val()}</p>
-                                        </div>
-                                    </div>`);
-                modal.style.display = 'none';
-            }
-            else {
-                alert('Please enter a vehicle name');
-            };
-        });
-
-        $('#vehicle-close').click(function () {
+    $('#vehicle-form').on('click', '#submit', function (event) {
+        event.preventDefault();
+        if ($('#vehicle-input').val() !== '') {
+            $('#results').append(`<div class="item vehicle">
+                                    <div class="picture">
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="iteminfo">
+                                        <p>${$('#vehicle-input').val()}</p>
+                                    </div>
+                                </div>`);
             modal.style.display = 'none';
-        });
+        }
+        else {
+            alert('Please enter a vehicle name');
+        };
+        $('#vehicle-input').val('');
+    });
+
+    $('#vehicle-close').click(function () {
+        modal.style.display = 'none';
     });
 };
 
 
 function addItem() {
-    $('#add-item').click(function () {
-        let modal = document.getElementById('addNewItem-modal');
+    let modal = document.getElementById('addNewItem-modal');
+    $('#add-item').click(function () {    
         modal.style.display = "block";
-
-        $('#item-form').on('click', '#submit', function (event) {
-            event.preventDefault();
-            $('#results').append(`<div class="item">
-                                        <div class="picture">
-                                            <img src="" alt="">
-                                        </div>
-                                        <div class="iteminfo">
-                                            <p>${$('#item-input').val()}</p>
-                                        </div>
-                                        <div class="iteminfo">
-                                            <p>$${$('#price-input').val()}</p>
-                                        </div> 
-                                        <div class="iteminfo">
-                                            <p>Quantity: ${$('#quantity-input').val()}</p>
-                                        </div> 
-                                   <div>`);
-            modal.style.display = 'none';
-        });
-
-        $('#item-close').click(function () {
-            modal.style.display = 'none';
-        });
     });
+
+    $('#item-form').on('click', '#submit', function (event) {
+        event.preventDefault();
+        $('#results').append(`<div class="item" id="item">
+                                    <div class="picture">
+                                        <img src="" alt="">
+                                    </div>
+                                    <div class="iteminfo">
+                                        <p>${$('#item-input').val()}</p>
+                                    </div>
+                                    <div class="iteminfo">
+                                        <p>$${$('#price-input').val()}</p>
+                                    </div> 
+                                    <div class="iteminfo">
+                                        <p>Quantity: ${$('#quantity-input').val()}</p>
+                                    </div> 
+                                <div>`);
+        modal.style.display = 'none';
+        $('#item-input').val('');
+        $('#price-input').val('');
+        $('#quantity-input').val('');
+    });
+
+    $('#item-close').click(function () {
+        modal.style.display = 'none';
+    });   
 };
+
+/*function editItem () {
+    let modal = document.getElementById('editItem-modal');
+    $('#results').on('click', '#item', function () {
+        console.log($(this).find('#name')[0].innerHTML);
+        $('#editItem-modal').append(`    
+            <div class="modal-content">
+                <span class="close" id="editItem-close">&times;</span>
+                <form action="" method="">
+                    <fieldset class="item-form" id="item-form">
+                        <legend>Edit Item</legend>
+                        <label for="item-image">Image</label>
+                        <input type="file" name="item-image" id="item-image">
+                        <label for="item-name">Item Name</label>
+                        <input type="text" value="${$(this).find('#name')[0].innerHTML}" id="item-input" class="item-input" required>
+                        <label for="part-number">Part Number</label>
+                        <input type="text" placeholder="SLL-12345" id="partnumber-input" class="item-input" required>
+                        <label for="list-price">List Price</label>
+                        <input type="number" value="${$(this).find('#price')[0].innerHTML}" id="price-input" class="item-input" required>
+                        <label for="quantity">Quantity On Hand</label>
+                        <input type="number" value="${$(this).find('#quantity')[0].innerHTML}" id="quantity-input" class="item-input" required>
+                        <label for="reorder-point">Minimum Amount</label>
+                        <input type="number" placeholder="6" id="reorder-input" class="item-input" required>
+                        <input type="submit" value="Submit" id="submit">
+                    </fieldset>
+                </form>
+            </div>`);
+        modal.style.display = "block";    
+    });
+    $('#editItem-modal').on('click', '#editItem-close', function () {
+        modal.style.display = 'none';
+        $('#editItem-modal').empty();
+    }); 
+};*/
 
