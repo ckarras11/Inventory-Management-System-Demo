e3728c55327a2614f328476ec4790bef24251e74
Christopher Karras <ckarras11@gmail.com>
2017-10-07 22:22:10 -0400

    Minor changes to item model

--- models/item.js
+++ models/item.js
@@ -6,7 +6,8 @@ const itemSchema = mongoose.Schema({
     partNumber: String,
     listPrice: {type: Number, required: true},
     quantityOnHand: {type: Number, required: true},
-    reorderPoint: {type: Number, required: true}
+    reorderPoint: {type: Number, required: true},
+    vehicle_id: {type: String, required: true}
 
 });
 
@@ -14,10 +15,12 @@ itemSchema.methods.apiRepr = function() {
     return {
         id: this.id,
         image: this.image,
+        item: this.item,
         partNumber: this.partNumber,
         listPrice: this.listPrice,
         quantityOnHand: this.quantityOnHand,
-        reorderPoint: this.reorderPoint
+        reorderPoint: this.reorderPoint,
+        vehicle_id: this.vehicle_id
     }
 };
 
