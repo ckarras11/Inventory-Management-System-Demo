2d92de97deed65ebaab18756820eed647b9a3179
Christopher Karras <ckarras11@gmail.com>
2017-11-05 21:18:38 -0500

    Changed alerts to ES6 arrow functions

--- public/Js/alerts.js
+++ public/Js/alerts.js
@@ -1,36 +1,36 @@
 // Vehicle Alerts
 function addVehicleAlert() {
     $('.title').append('<div class="alert alert-success">Vehicle Added Succesfully</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function removeVehicleAlert() {
     $('.title').append('<div class="alert alert-info">Vehicle Removed Succesfully</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function editVehicleAlert() {
     $('.title').append('<div class="alert alert-info">Vehicle Updated</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function vehicleErrorAlert(message) {
     $('#vehicle-form').prepend(`<div class="alert alert-danger">${message}</div>`);
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 5000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 5000);
 }
 
 // Item Alerts
 function addItemAlert() {
     $('.title').append('<div class="alert alert-success">Item Added Succesfully</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function removeItemAlert() {
     $('.title').append('<div class="alert alert-info">Item Removed Succesfully</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function editItemAlert() {
     $('.title').append('<div class="alert alert-info">Item Updated</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
--- public/js/prod/min.js
+++ public/js/prod/min.js
@@ -1,38 +1,38 @@
 // Vehicle Alerts
 function addVehicleAlert() {
     $('.title').append('<div class="alert alert-success">Vehicle Added Succesfully</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function removeVehicleAlert() {
     $('.title').append('<div class="alert alert-info">Vehicle Removed Succesfully</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function editVehicleAlert() {
     $('.title').append('<div class="alert alert-info">Vehicle Updated</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function vehicleErrorAlert(message) {
     $('#vehicle-form').prepend(`<div class="alert alert-danger">${message}</div>`);
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 5000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 5000);
 }
 
 // Item Alerts
 function addItemAlert() {
     $('.title').append('<div class="alert alert-success">Item Added Succesfully</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function removeItemAlert() {
     $('.title').append('<div class="alert alert-info">Item Removed Succesfully</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 function editItemAlert() {
     $('.title').append('<div class="alert alert-info">Item Updated</div>');
-    setTimeout(function () { $('.alert').addClass('js-hide-display'); }, 2000);
+    setTimeout(() => { $('.alert').addClass('js-hide-display'); }, 2000);
 }
 
 let isVehicle = true;
@@ -256,7 +256,7 @@ function deleteItem(data) {
                 url: `/api/inventory/${data.id}`,
                 success: () => {
                     $(`#${data.id}`).remove();
-                    modal.style.display = 'none';
+                    hideEditItemModal();
                     removeItemAlert();
                 },
             });
